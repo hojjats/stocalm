65eebd3b3bd0384a3de0a4445e3463376f8c11f2
tim <timwahlgren@live.se>
2019-04-18 14:50:45 +0200

    Add test view to make API call to backend

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {ApiService} from '../shared/api.service';
+import {Sensor} from '../shared/models/sensor.model';
+import {Reading} from '../shared/models/reading.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+
+  readings: Reading[] = [];
+  subscriptions: Subscription[] = [];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  public onSearchSensor(value: string) {
+    this.readings = []; // Set init
+    if (!!value) {
+      const id = Number.parseInt(value, 10);
+      this.apiService.getReadingsBySensorId(id)
+        .subscribe((sensor: Sensor) => {
+          if (!!sensor) {
+            this.readings = sensor.readings;
+          }
+        });
+    }
+  }
+
 
 }
+
