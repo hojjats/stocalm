7dbff6dc9866bbfeb1f726e633f3df2f05003a82
Hojjat Seyed <hojjatseyed@gmail.com>
2019-05-07 13:48:34 +0200

    range fixes and styling

--- src/app/advanced-search/advanced-search.page.ts
+++ src/app/advanced-search/advanced-search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../shared/services/api.service';
+import {Sensor} from '../shared/models/sensor.model';
 
 @Component({
   selector: 'app-advanced-search',
@@ -6,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-search.page.scss'],
 })
 export class AdvancedSearchPage implements OnInit {
+  filteredsensors: Sensor[];
   decibel: any;
   min: any = 0;
   max: any = 140;
   min2: any;
   max2: any;
 
-  constructor() {}
+  constructor(public apiservice: ApiService) {
+  }
 
   ngOnInit() {
     this.decibel = 140;
@@ -23,5 +27,9 @@ export class AdvancedSearchPage implements OnInit {
   change() {
     this.min2 = this.decibel.lower;
     this.max2 = this.decibel.upper;
+  }
+
+  filter() {
+
   }
 }
