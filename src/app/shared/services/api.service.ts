66158a794a9e8dc67b6933bac17102612abcedb3
Hojjat Seyed <hojjatseyed@gmail.com>
2019-05-13 15:30:08 +0200

    Issues functionality add

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -6,41 +6,58 @@ import {Subject} from 'rxjs';
 import {map} from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
 
-  sensors: Sensor[] = [];
-  sensors$: Subject<Sensor[]> = new Subject();
+    sensors: Sensor[] = [];
+    sensors$: Subject<Sensor[]> = new Subject();
 
-  constructor(private httpClient: HttpClient) {
-    this.getSensors().pipe(
-      map((data: any[]) => {
-        return data.map(item => {
-          return <Sensor>{
-            id: item.id,
-            coords: item.position,
-            readings: item.readings
-          };
+    constructor(private httpClient: HttpClient) {
+        this.getSensors().pipe(
+            map((data: any[]) => {
+                return data.map(item => {
+                    return <Sensor>{
+                        id: item.id,
+                        coords: item.position,
+                        readings: item.readings
+                    };
+                });
+            })
+        ).subscribe(next => {
+            const sensors = <Sensor[]><unknown>next;
+            sensors.forEach(sensor => this.sortReadings(sensor));
+            this.sensors = sensors;
+            this.sensors$.next(this.sensors);
         });
-      })
-    ).subscribe(next => {
-      const sensors = <Sensor[]><unknown>next;
-      this.sensors = sensors;
-      this.sensors$.next(sensors);
-    });
-  }
+    }
 
-  getReadingsBySensorId(id: number) {
-    return this.httpClient.get(environment.BASE_URL + 'sensors/' + id);
-  }
+    sortReadings(sensor) {
+        sensor.readings = sensor.readings.sort(((a, b) => {
+            if (a.date < b.date) {
+                return 1;
+            } else if (a.date > b.date) {
+                return -1;
+            } else if (a.time.substring(0, 5) < b.time.substring(0, 5)) {
+                return 1;
+            } else if (a.time.substring(0, 5) > b.time.substring(0, 5)) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }));
+    }
 
-  getSensors() {
-    return this.httpClient.get(environment.BASE_URL + 'sensors');
-  }
+    getReadingsBySensorId(id: number) {
+        return this.httpClient.get(environment.BASE_URL + 'sensors/' + id);
+    }
 
-  test() {
-    this.httpClient.get(environment.BASE_URL + 'sensors/graph/weekdays',);
-  }
+    getSensors() {
+        return this.httpClient.get(environment.BASE_URL + 'sensors');
+    }
+
+    test() {
+        this.httpClient.get(environment.BASE_URL + 'sensors/graph/weekdays',);
+    }
 
 }
